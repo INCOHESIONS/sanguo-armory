ed98726b7635aa6c4a48bad5ce3285b1ede9e89a
iNCOHESiONS <incohesions@gmail.com>
2025-09-04 03:08:39 -0300

    lore, effect and attrmod changes

--- src/main/kotlin/io/github/incohesions/sanguo_armory/extensions/ItemExtensions.kt
+++ src/main/kotlin/io/github/incohesions/sanguo_armory/extensions/ItemExtensions.kt
@@ -6,7 +6,6 @@ import net.minecraft.component.ComponentType
 import net.minecraft.component.DataComponentTypes
 import net.minecraft.component.type.AttributeModifierSlot
 import net.minecraft.component.type.AttributeModifiersComponent
-import net.minecraft.component.type.AttributeModifiersComponent.Entry as AttributeModifier
 import net.minecraft.component.type.DamageResistantComponent
 import net.minecraft.component.type.LoreComponent
 import net.minecraft.entity.attribute.EntityAttribute
@@ -21,8 +20,8 @@ import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 import net.minecraft.util.Unit
 
-fun Item.Settings.lore(vararg lines: Text): Item.Settings =
-    component(DataComponentTypes.LORE, LoreComponent(lines.toList()))
+fun Item.Settings.lore(vararg styledLines: Text): Item.Settings =
+    component(DataComponentTypes.LORE, LoreComponent(emptyList(), styledLines.toList()))
 
 fun Item.Settings.cooldown(cooldown: Float?): Item.Settings {
     return useCooldown(cooldown ?: return this)
@@ -55,12 +54,16 @@ fun Item.Settings.indestructible(): Item.Settings = this
 fun Item.Settings.effect(id: String, amplifier: Int = 0): Item.Settings =
     component(HeldItemEffectComponent.TYPE, HeldItemEffectComponent(Identifier.ofVanilla(id), amplifier))
 
-fun Item.Settings.modifier(
+fun Item.Settings.effect(effect: Pair<String, Int>?): Item.Settings {
+    return effect((effect ?: return this).first, effect.second)
+}
+
+// WARN: Should be used only AFTER .tool, .sword or any other similar methods since they also use modifiers.
+fun AttributeModifiersComponent.Builder.add(
     attr: RegistryEntry<EntityAttribute>,
-    value: Double,
+    id: Identifier? = null,
+    value: Double = 0.0,
     operation: Operation = Operation.ADD_VALUE,
     slot: AttributeModifierSlot = AttributeModifierSlot.MAINHAND
-): Item.Settings {
-    val mod = AttributeModifier(attr, EntityAttributeModifier(attr.key.get().registry, value, operation), slot)
-    return attributeModifiers(AttributeModifiersComponent(listOf(mod)))
-}
+): AttributeModifiersComponent.Builder =
+    add(attr, EntityAttributeModifier(id ?: attr.key.get().registry, value, operation), slot)
